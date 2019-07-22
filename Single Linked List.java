d4eaac724121c311736bc9909240f1121ba3c082
Radiansyah Akbar <53164025+barb4r@users.noreply.github.com>
2019-07-22 17:01:00 +0700

    Single Linked List

---
+++ Single Linked List.java
@@ -0,0 +1,23 @@
+run:
+Masukkan angka sebanyak 5 kali (jangan lupa di enter dahulu)
+2
+5
+6
+7
+8
+
+LinkedList: 2 5 6 7 8 
+
+Delete angka by Key (Angka yang tertera diatas) 
+2
+2 found and deleted
+
+LinkedList: 5 6 7 8 
+
+Delete angka by Position (1/2/3/4) 
+2
+2 position element deleted
+
+LinkedList: 5 7 8 
+
+BUILD SUCCESSFUL (total time: 15 seconds)
